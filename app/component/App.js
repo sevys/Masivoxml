8930d6190226954c6a4db75af6acab8934295504
sevys <201228@ids.upchiapas.edu.mx>
2021-12-15 00:38:30 -0600

    completo

---
+++ app/component/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component{
+    render() {
+        return(
+
+            <BrowserRouter>
+                <Switch>
+
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
+
+
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
