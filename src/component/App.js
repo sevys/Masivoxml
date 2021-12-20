b20d20adc23120c8668184a0657eb0aa6c60143c
sevys <201228@ids.upchiapas.edu.mx>
2021-12-20 13:34:33 -0600

    cambio de dependencias

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {BrowserRouter,Route, Switch} from "react-router-dom";
+import {BrowserRouter,HashRouter,Route, Switch} from "react-router-dom";
 import home from "../page/home";
 import Basic from "../page/Basic";
 import Premium from "../page/Premium";
+import Videotutorial from "./Videotutorial";
 class App extends React.Component{
     render() {
         return(
@@ -12,6 +13,7 @@ class App extends React.Component{
                     <Route exact path='/' component={home}/>
                     <Route exact path='/Basic' component={Basic}/>
                     <Route exact path='/Premium' component={Premium}/>
+                    <Route exact path='/Videotutoriales' component={Videotutorial}/>
                 </Switch>
             </BrowserRouter>
         )
--- src/component/Videotutorial.js
+++ src/component/Videotutorial.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
+import '../assets/stylesheet/video.css';
 
 class Videotutorial extends React.Component{
     constructor(props) {
@@ -8,6 +11,14 @@ class Videotutorial extends React.Component{
     render(){
         return(
             <>
+                <section><Header/></section>
+                <div className="video">
+                    <iframe src="https://www.youtube.com/embed/aQpJ37JLUio"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                </div>
+                <section><Footer/></section>
             </>
         )
     }
