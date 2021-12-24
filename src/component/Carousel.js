0c694caccf3990b363a9b5cde9b42ef1520619e3
Tu Nombre <you@example.com>
2021-12-24 10:31:10 -0600

    button

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -16,23 +16,18 @@ class Carousel extends React.Component{
                             <div className="image-container d-flex justify-content-center align-items-center flex-column" style={{backgroundImage: "url(" + carousel + ")"}}>
 
                                 <h5 className="pes">¿QUÉ ES  +  IVO XML?</h5>
-                                <br/>
                                 <div className="linea-1"></div>
-                                <br/>
                                 <p className="text-carousel">+IVO XML es un programa contable,
                                     que te permite tener el control de las facturas<br/>
                                     emitidas y recibidas en tu negocio,
                                     lo cual facilita la administración y el<br/>
                                     cumplimiento de tus obligaciones fiscales.</p>
-                                <br/>
                                 <button className="btn-carousel">OBTENER</button>
 
                             </div>
                             <div className="image-container d-flex justify-content-center align-items-center flex-column" style={{backgroundImage: "url(" + carousel + ")"}}>
                                 <h5 className="pes">Requisitos de Instalación</h5>
-                                <br/>
                                 <div className="linea-2"></div>
-                                <br/>
                                 <p className="text-carousel">Sistema Operativo Windows 7, 8, 8.1, 10
                                     <br/>
                                     Internet Explorer 10 u 11
